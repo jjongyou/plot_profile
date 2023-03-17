c7c8a840463a51b35160e4c5b6379265181113e1
jjongyou <youjong12@naver.com>
2023-03-17 15:00:04 +0900

    plotting data python program

--- paper_plot.py
+++ paper_plot.py
@@ -1,34 +1,78 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Data
 paths = [
-        "normal_usage",
+        "no_recovery_usage",
         "reexecute_usage",
-        "util_usage",
         "unutil_usage",
-        "replica_no_util_usage",
-        "replica_yes_util_usage"
+        "util_usage"
         ]
+colors = [
+         "black",
+         "orange",
+         "red",
+         "blue"
+         ]
+markers = [
+         "o",
+         "o",
+         "*",
+         "o"
+         ]
+
+bound = 30
+timestamp = 0
+timestep = 0.5
+
+# Plot
+marker_size = 5
+fig_x = 10
+fig_y = 5
+font_size = 24
+x_label = "Time (secs)"
+y_label = "CPU usage (%)"
+y_min = 0
+y_max = 100
+fig_name = "cpu_usage_data.png"
 
 data_list = []
-idx = float(0.5)
 idx_list = []
-datas = open("../" + paths[5] + ".txt")
-for line in datas.readlines():
-  print(line[11:-55])
-  data = line[11:-55]
-  data_list.append(float(data))
-  idx_list.append(float(idx))
-  idx += 0.5
-  if idx == 30:
-    break
-plt.figure(figsize=(10, 5))
-plt.xlabel('Time (secs)', fontsize=24)
-plt.ylabel('CPU usage (%)', fontsize=24)
-plt.ylim(0, 100)
+timestamps = []
 
-plt.plot(idx_list, data_list, color = "red")
-# plt.plot(np.array(idx_list), np.array(data_list), color = "red")
-# plt.legend()
+profile_data = {}
+
+for path in paths:
+  datas = open("../" + path + ".txt")
+  for line in datas.readlines():
+    # print(line[11:-55])
+    data = line[11:-55]
+
+    data_list.append(float(data))
+    idx_list.append(float(timestamp))
+    timestamp += timestep
+
+    if timestamp == bound:
+      profile_data[path] = data_list.copy()
+      if not timestamps:
+        timestamps = idx_list.copy()
+      idx_list.clear()
+      data_list.clear()
+      timestamp = 0
+      break
+
+plt.figure(figsize=(fig_x, fig_y))
+plt.xlabel(x_label, fontsize=font_size)
+plt.ylabel(y_label, fontsize=font_size)
+plt.ylim(y_min, y_max)
+
+for key, value, mark, col in zip(profile_data.keys(), profile_data.values(), markers, colors):
+# for key, value, col in profile_data.items():
+  # random color
+  # col = (np.random.random(), np.random.random(), np.random.random())
+  plt.plot(timestamps, value, marker=mark, markersize=marker_size, color=col, label=key)
+
+plt.legend()
 plt.tight_layout()
+plt.savefig(fig_name)
 plt.show()
